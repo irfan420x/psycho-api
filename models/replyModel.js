871ec99c8ab82ebfda39b46ad6f6374844e4f11f
irfan420x <marufmdirfanahmed@gmail.com>
2025-03-08 00:13:04 +0800

    Update replyModel.js

---
+++ models/replyModel.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const replySchema = new Schema({
+    message: {
+        type: String,
+        required: true
+    },
+    replies: [{
+        type: String
+    }],
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model('Reply', replySchema);
